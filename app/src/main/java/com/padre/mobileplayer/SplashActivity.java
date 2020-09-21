0ec520ccba00e446be937a35176208adf5c636e7
José Antonio <padre_japg@163.com>
2020-09-21 11:00:16 -0500

    Init and Splash

---
+++ app/src/main/java/com/padre/mobileplayer/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.padre.mobileplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
